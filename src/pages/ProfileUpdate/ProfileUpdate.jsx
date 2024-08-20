c6ef878ab9672349d86ea646d9eb15ea96ab4f67
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2024-08-19 19:57:29 -0500

    Funcionalidad y Estética del perfil de usuario

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../ProfileUpdate/ProfileUpdate.css'
+import assets from '../../assets/assets'
 
 const ProfileUpdate = () => {
+
+    const [image, setImage] = useState(false);
+
     return (
-        <div>
-            Profile
+        <div className='profile'>
+            <div className="profile-container">
+                <form>
+                    <h3>Detalles del Perfil</h3>
+                    <label htmlFor="avatar">
+                        <input onChange={(e)=>setImage(e.target.files[0])} type="file" id="avatar" accept='.png, .jpg, .jpeg' hidden/>
+                        <img src={image? URL.createObjectURL(image) : assets.avatar_icon} alt="" />
+                        Actualizar foto de perfil
+                    </label>
+                    <input type="text" placeholder='Su nombre' required/>
+                    <textarea placeholder='Escriba en su biografia' required></textarea>
+                    <button type='submit'>Guardar</button>
+                </form>
+                <img className='profile-pic' src={image? URL.createObjectURL(image) : assets.logo_icon} alt="" />
+            </div>
         </div>
     )
 }
